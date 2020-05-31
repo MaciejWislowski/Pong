83e821c20a8d7ef6d5747d0ed025cc0ef252d7ac
Maciej <maciej.wislowski@gmail.com>
2020-05-31 12:05:51 +0100

    Update 0.3

--- script.js
+++ script.js
@@ -28,15 +28,23 @@ const lineHeight = 16;
 let ballSpeedX = -4;
 let ballSpeedY = -4;
 
+let playerScore = 0;
+let aiScore = 0;
+
+let playerSet = 0;
+let aiSet = 0;
+
+let ballSpeedMax = 15;
+
+
 topCanvas = canvas.offsetTop;
 console.log(topCanvas);
 
-// Functions
-function player() {
-    ctx.fillStyle = "grey";
-    ctx.fillRect(playerX, playerY, paddleWidth, paddleHeight); 
-}
+//---------------------------------------------------------------------------------------------//
+//---------------------------------    Functions   --------------------------------------------//
+//---------------------------------------------------------------------------------------------//
 
+// --------------------------------   Draw on canvas ------------------------------------------//
 function ai() { 
     ctx.fillStyle = "darkgrey";
     ctx.fillRect(aiX, aiY, paddleWidth, paddleHeight); 
@@ -48,59 +56,12 @@ function ball() {
 
     ballX += ballSpeedX;
     ballY += ballSpeedY;
-
-    if (ballY <= 0 || ballY >=ch-ballSize) {
-        ballSpeedY = -ballSpeedY;
-    }
-    if (ballX <= 0) {
-        window.location.reload();
-    }
-    if (ballX >=cw-ballSize) {
-        window.location.reload();
-    }
-
-    if ((ballX <= playerX + paddleWidth) && (ballX > playerX + paddleWidth-15)  && (ballY + ballSize> playerY) && (ballY< playerY + paddleHeight)) {
-        ballX += 5;
-        ballSpeedX = -ballSpeedX;
-        ballSpeedY = ballSpeedY;
-        speedUp();
-    }
-    if (((ballX < playerX + paddleWidth) && (ballX+ballSize > playerX)) && (ballY + ballSize >= playerY) && (ballY + ballSize <= playerY+9) ) {
-        ballX += 5;
-        ballY -= 5;
-    ballSpeedX = -ballSpeedX;
-    ballSpeedY = -ballSpeedY;
-    speedUp();
-    }
-    if (((ballX < playerX + paddleWidth) && (ballX+ballSize > playerX)) && (ballY <= playerY + paddleHeight) && (ballY > playerY + paddleHeight-9) ) {
-        ballX += 5;
-        ballY += 5;
-        ballSpeedX = -ballSpeedX;
-        ballSpeedY = -ballSpeedY;
-    speedUp();
-    }
-    if ((ballX + ballSize >= aiX) && (ballX + ballSize < aiX+10) && (ballY + ballSize >= aiY) && (ballY <= aiY + paddleHeight)) {
-        ballX -= 5;
-        ballSpeedX = -ballSpeedX;
-        ballSpeedY = ballSpeedY;
-        speedUp();
-    }
-    if ((ballX + ballSize > aiX ) && (ballX + ballSize < aiX + paddleWidth) && (ballY + ballSize >= aiY) && (ballY + ballSize <= aiY+9) ) {
-        ballX -= 5;
-        ballY -= 5;
-        ballSpeedX = -ballSpeedX;
-        ballSpeedY = -ballSpeedY;
-    speedUp();
-    }
-    if ((ballX + ballSize > aiX ) && (ballX + ballSize < aiX + paddleWidth)&& (ballY <= aiY + paddleHeight) && (ballY > aiY + paddleHeight-9) ) {
-        ballX -= 5;
-        ballY += 5;
-        ballSpeedX = -ballSpeedX;
-        ballSpeedY = -ballSpeedY;
-        speedUp();  
-    }
 }
 
+function player() {
+    ctx.fillStyle = "grey";
+    ctx.fillRect(playerX, playerY, paddleWidth, paddleHeight); 
+}
 
 function table() {
     // board
@@ -114,16 +75,114 @@ function table() {
 }
 
 
+//-----------------------------------------   Mechanics   ------------------------------------//
 
-function game () {
+// AI paddle position on Y Axis
+function aiPosition () {
+    var middlePaddle = aiY + paddleHeight/2;
+    var middleBall = ballY + ballSize/2;
 
-    table()
-    ball()
-    player()
-    ai()
-    aiPosition()
+   // Ai paddle movement on ai part of board
+    if(ballX > cw/2) {
+        if (middlePaddle - middleBall > 200) {
+            aiY -= ballSpeedMax+playerScore/11*(playerSet+1)-aiScore/11; 
+        }
+        else if (middlePaddle - middleBall > 50) {
+            aiY -= ballSpeedMax/3+playerScore/11*(playerSet+1)-aiScore/11;
+        }
+        else if (middlePaddle - middleBall < -200) {
+            aiY += ballSpeedMax+playerScore/11*(playerSet+1)-aiScore/11;
+        }
+        else if (middlePaddle - middleBall < -50) {
+            aiY += ballSpeedMax/3+playerScore/11*(playerSet+1)-aiScore/11dada;
+        }
+    }
+
+    // AI movement on players part of board
+    else if (ballX <= cw/2 && ballX > 2*playerX) {
+        if (middlePaddle-middleBall > 100) {
+            aiY -= ballSpeedMax/5;
+        }
+        else if (middlePaddle - middleBall < - 100) {
+            aiY += ballSpeedMax/5;
+        }
+    }
 }
 
+// All collisions set up
+function collistions() {
+
+    // Colisions with top and bottom line
+    if (ballY <= 0 || ballY >=ch-ballSize) {
+        ballSpeedY = -ballSpeedY;
+    }
+
+    // Collisions with left and right line
+    if (ballX <= 0) {
+        ballX = cw/2-ballSize/2;
+        ballY = ch/2-ballSize/2;
+        ballSpeedX = 4;
+        ballSpeedY = 4;
+        aiScore++;
+        document.getElementById('0002').innerHTML = aiScore;
+    }
+    if (ballX >=cw-ballSize) {
+        ballX = cw/2-ballSize/2;
+        ballY = ch/2-ballSize/2;
+        ballSpeedX = -4;
+        ballSpeedY = -4;
+        playerScore++;
+        document.getElementById('0001').innerHTML = playerScore;
+    }
+
+    // Collisions with Players paddle
+    if ((ballX <= playerX + paddleWidth) && (ballX > playerX + paddleWidth-ballSpeedMax)  && (ballY + ballSize> playerY) && (ballY< playerY + paddleHeight)) {
+        ballX += 5;
+        ballSpeedX = -ballSpeedX;
+        ballSpeedY = ballSpeedY;
+        speedUp();
+    }
+    if (((ballX < playerX + paddleWidth) && (ballX+ballSize > playerX)) && (ballY + ballSize >= playerY) && (ballY + ballSize <= playerY+9) ) {
+        ballX += 10;
+    ballSpeedX = -ballSpeedX;
+    ballSpeedY = -ballSpeedY;
+    speedUp();
+    }
+    if (((ballX < playerX + paddleWidth) && (ballX+ballSize > playerX)) && (ballY <= playerY + paddleHeight) && (ballY > playerY + paddleHeight-9) ) {
+        ballX += 10;
+        ballSpeedX = -ballSpeedX;
+        ballSpeedY = -ballSpeedY;
+    speedUp();
+    }
+
+    // Colisions with Ai paddle
+    if ((ballX + ballSize >= aiX) && (ballX + ballSize < aiX+ballSpeedMax) && (ballY + ballSize >= aiY) && (ballY <= aiY + paddleHeight)) {
+        ballX -= 5;
+        ballSpeedX = -ballSpeedX;
+        ballSpeedY = ballSpeedY;
+        speedUp();
+    }
+    if ((ballX + ballSize > aiX ) && (ballX + ballSize < aiX + paddleWidth) && (ballY + ballSize >= aiY) && (ballY + ballSize <= aiY+9) ) {
+        ballX -= 10;
+        ballSpeedX = -ballSpeedX;
+        ballSpeedY = -ballSpeedY;
+    speedUp();
+    }
+    if ((ballX + ballSize > aiX ) && (ballX + ballSize < aiX + paddleWidth)&& (ballY <= aiY + paddleHeight) && (ballY > aiY + paddleHeight-9) ) {
+        ballX -= 10;
+        ballSpeedX = -ballSpeedX;
+        ballSpeedY = -ballSpeedY;
+        speedUp();  
+    }
+}
+
+function overpower1() {
+    if (playerScore>=9) {
+        overpower++;
+    }
+}
+
+// Players paddle position on Y Axis
 function playerPosition(e) {
     // console.log("Pozycja myszy to: " (e.clientY- topCanvas));
     playerY = e.clientY - topCanvas - paddleHeight/2;
@@ -137,49 +196,58 @@ function playerPosition(e) {
     }
 }
 
-function aiPosition () {
-    var middlePaddle = aiY + paddleHeight/2;
-    var middleBall = ballY + ballSize/2;
-
-    if(ballX > cw/2) {
-        if (middlePaddle - middleBall > 200) {
-            aiY -= 15; 
-        }
-        else if (middlePaddle - middleBall > 50) {
-            aiY -= 5;
-        }
-        else if (middlePaddle - middleBall < -200) {
-            aiY += 15;
-        }
-        else if (middlePaddle - middleBall < -50) {
-            aiY += 5;
-        }
-    }
-    else if (ballX <= cw/2 && ballX > 2*playerX) {
-        if (middlePaddle-middleBall > 100) {
-            aiY -= 2;
-        }
-        else if (middlePaddle - middleBall < - 100) {
-            aiY += 2;
-        }
-    }
-
-}
-
+// Ball speed change
 function speedUp() {
-    if (ballSpeedX >0 && ballSpeedX < 15) {
+    if (ballSpeedX >0 && ballSpeedX < ballSpeedMax) {
         ballSpeedX += 0.5;
     }
-    else if (ballSpeedX < 0 && ballSpeedX > -15) {
+    else if (ballSpeedX < 0 && ballSpeedX > -ballSpeedMax) {
         ballSpeedX -= 0.5;
     }
-    if (ballSpeedY >0 && ballSpeedY < 15) {
+    if (ballSpeedY >0 && ballSpeedY < ballSpeedMax) {
         ballSpeedY += 0.1;
     }
-    else if (ballSpeedY < 0 && ballSpeedY > -15) {
+    else if (ballSpeedY < 0 && ballSpeedY > -ballSpeedMax) {
         ballSpeedY -= 0.1;
+    }  
+}
+
+// Set and victory conditions
+function set() {
+    if(playerScore >= 11 && playerScore - aiScore >= 2) {
+        playerSet++;
+        playerScore = 0;
+        aiScore = 0;
+        document.getElementById('0003').innerHTML = playerSet;
+        document.getElementById('0002').innerHTML = aiScore;
+        document.getElementById('0001').innerHTML = playerScore;
+        ballSpeedMax++
     }
-    
+    if(aiScore >= 11 && aiScore - playerScore >= 2 ) {
+        aiSet++;
+        playerScore -= playerScore;
+        aiScore -= aiScore;
+        ballSpeedMax++
+        document.getElementById('0004').innerHTML = aiSet;
+        document.getElementById('0002').innerHTML = aiScore;
+        document.getElementById('0001').innerHTML = playerScore;
+    }
+    if (playerSet==3) {
+        alert("Congratulations!!! You win!!!");
+    }
+    if (aiSet==3) {
+        alert("You are a looser!!!");
+    }
+}
+
+function game () {
+    table();
+    ball();
+    player();
+    ai();
+    aiPosition();
+    collistions();
+    set();
 }
 
 //Event listener
